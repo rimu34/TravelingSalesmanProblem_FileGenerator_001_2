04fbb8e04543ac4ac23716d3241a5a3c831efe04
rimu34 <50013418+rimu34@users.noreply.github.com>
2023-04-24 00:01:52 +0900

    Add project files.

---
+++ TFG.Core/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace TFG.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Prism.Ioc;
+
+    public static class DependencyInjection
+    {
+        public static IContainerRegistry RegisterCoreServices(this IContainerRegistry registry)
+        {
+
+            return registry;
+        }
+    }
+}
---
+++ TFG/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace TFG
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using Prism.DryIoc;
+    using Prism.Ioc;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
